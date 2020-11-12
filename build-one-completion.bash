84cb7330150ce550393b8418a7a6802e80da30bd
Fred Tingaud <ftingaud@hotmail.com>
2020-11-12 16:22:59 +0000

    Remove deprecated scripts.

--- build-one-completion.bash
+++
@@ -1,8 +0,0 @@
-#/bin/env bash
-
-_completion()
-{
-  COMPREPLY=($(compgen -W "-f --force $(git branch --list 'clang-*' 'gcc-*' --format='%(refname:short)' | sed ':a;N;$!ba;s/\n/ /g')" -- "${COMP_WORDS[1]}"))
-}
-
-complete -F _completion build-one
